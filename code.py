9963285303002953285b719a120a822991483e38
Kawsar Uddin Ahmed <52331860+Kawsar-Uddin-Ahmed@users.noreply.github.com>
2022-06-14 21:31:23 +0600

    Add files via upload

---
+++ code.py
@@ -0,0 +1,18 @@
+import time
+a = open("app.bat","w")
+a.write("reg query HKEY_CURRENT_USER\SOFTWARE\>>software_list.txt")
+a.close()
+time.sleep(10) #Within this 10 second please double click manually on the batch file that is created
+try:
+    for f in  open('software_list.txt'):
+         a = f.replace("\\" ," ")
+         b = a.split()
+         c = len(b)
+         for i in range(c):
+              d = b[i + (c - 1)]
+              with open("only_software_name", "a") as f:
+                   print(d,file=f)
+              break
+except(FileNotFoundError):
+     print("Please press the batch file manually")
+
