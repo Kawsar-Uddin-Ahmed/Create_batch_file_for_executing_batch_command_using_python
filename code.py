bbf7a69d990df08a085304671e961caa41524e0d
Kawsar Uddin Ahmed <52331860+Kawsar-Uddin-Ahmed@users.noreply.github.com>
2023-07-03 23:06:20 +0600

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ import os
 a = open("app.bat","w")
 a.write("reg query HKEY_CURRENT_USER\SOFTWARE\>>software_list.txt")
 a.close()
-#time.sleep(10) #Within this 10 second please double click manually on the batch file that is created
+#time.sleep(10) #Within these 10 seconds please double-click manually on the batch file that is created
 os.system("app.bat")
 try:
     for f in  open('software_list.txt'):
@@ -13,7 +13,7 @@ try:
          for i in range(c):
               d = b[i + (c - 1)]
               with open("only_software_name.txt", "a") as f:
-                   print(d,file=f)
+                   print(d,file=f) #Here file=f is used so that the input can be written in the created file. 
               break
 except(FileNotFoundError):
      print("Please press the batch file manually within 10 second")
